ff7d584801b38875d258bda8212d7454e2f6b56c
Wesley28w <wesleycwong@gmail.com>
2026-09-24 15:05:12 -0700

    shutdown + past threshold function

--- main.py
+++ main.py
@@ -11,12 +11,32 @@ FOCAL_LENGTH = STREAM_W / (2 * math.tan(FOV_H / 2))
 FOV_V = 2 * math.atan(STREAM_H / (2 * FOCAL_LENGTH)) # focal length is same for V/H FOV
 
 # TODO: make this work for kids too (maybe use ratio of both?)
+# LIMITATION: if someone is too close to camera where you can't see entire body then distance will be off. But should disable regardless.
 HUMAN_HEIGHT = 71.0 # inches - 5'11"
 HUMAN_WIDTH = 16.1 # inches - Shoulder SPAN
 
+# Calculation Constants
+WEIGHT_H = 0.6 # How much to weigh height distance calculation over width
+SCREEN_COVER_THRESHOLD = 0.5 # How much of the screen a person covers to be considered too close
+DISTANCE_THRESHOLD = 120 # inches. 10 feet
 model = YOLO('yolo11n.pt')
 
+# track whether shutdown or not
+shutdown = False
+
 cap = cv2.VideoCapture(0) # 0 is for video cam
+
+def past_threshold(distance_w: float, distance_h: float, screen_cover_ratio: float, confidence: float):
+    # if someone is too close (where their whole body is not even showing) we want to default to True
+    if screen_cover_ratio > SCREEN_COVER_THRESHOLD: return True
+
+    distance = (WEIGHT_H * distance_h) + ((1 - WEIGHT_H) * distance_w) # consolidate distances. Weighted
+    # use confidence to reduce distance in order to stay safer on less confident predictions
+    distance *= (confidence) # Can make this confidence scaling exponential, or weighted in future
+    
+    # if distance is too far 
+    if distance < DISTANCE_THRESHOLD: return True
+    return False
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -31,28 +51,21 @@ while cap.isOpened():
         annotated_frame = result.plot()
         xywh = result.boxes.xywh # center x/y, width, height
         width, height = xywh[2], xywh[3] # in pixels
-        
-        D_h = HUMAN_HEIGHT * (height / STREAM_H) # distance using height
-        D_w = HUMAN_WIDTH * (width / STREAM_W) # distance using width
-
-        
-
-        # math to convert into relative yaw distance
-
-
         conf = result.boxes.conf # confidence
-
         
-        # we want to consider humans CLOSER if confidence is low to be safer
+        d_h = HUMAN_HEIGHT * (height / STREAM_H) # distance using height
+        d_w = HUMAN_WIDTH * (width / STREAM_W) # distance using width
+        screenCoverRatio = (width * height) / (STREAM_W * STREAM_H)
+
+        shutdown = past_threshold(d_w, d_h, screenCoverRatio, conf)
+        if (shutdown): break
 
     cv2.imshow("Camera Stream", annotated_frame)
 
     # pauses execution for 1 ms delay, checks for q key press (quits)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if (cv2.waitKey(1) and 0xFF == ord("q")) or shutdown:
         break
-
     
-
 # clean up
 cap.release()
 cv2.destroyAllWindows()
